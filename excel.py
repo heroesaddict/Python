8c3184463c9d3287afc1697a8c7495ed17c43bd2
heroesaddict <chattokim@gmail.com>
2016-06-23 19:10:04 +0800

    Delete excel.py

--- excel.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import Workbook
-from openpyxl.compat import range
-wb = Workbook()
-ws1 = wb.active
-from openpyxl import load_workbook
-#wb = load_workbook(filename = 'empty_book.xlsx')
-#sheet_ranges = wb['Data']
-#print(sheet_ranges['AA19'].value)
